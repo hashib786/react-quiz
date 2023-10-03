4db942a0bcb66f8ad3c03db7eaf44a3a02751fdc
Hashib Raja <hashibraja23@gmail.com>
2023-10-03 15:48:13 +0530

    Added reducers in our app and defining some types in typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,51 @@
-import { useEffect } from "react";
+import { useEffect, useReducer } from "react";
 import Content from "./Content";
 import Header from "./Header";
 
+interface QuestionI {
+  correctOption: number;
+  options: string[];
+  points: number;
+  question: string;
+}
+
+type StatusT = "Loading" | "Error" | "Active" | "Ready" | "Finished";
+
+interface IntialStateI {
+  questions: QuestionI[];
+  status: StatusT;
+}
+
+const intialState: IntialStateI = {
+  questions: [],
+  status: "Loading",
+};
+
+type DataReceived = { type: "dataReceived"; payload: QuestionI[] };
+type ActionType = DataReceived;
+
+const reducers = (state: IntialStateI, action: ActionType): IntialStateI => {
+  const { type, payload } = action;
+  switch (type) {
+    case "dataReceived":
+      return { ...state, questions: payload };
+
+    default:
+      return state;
+  }
+};
+
 const App = () => {
+  const [state, dispatch] = useReducer(reducers, intialState);
+  console.log(state, dispatch);
+
   useEffect(() => {
     fetch("http://localhost:9000/questions")
       .then((res) => res.json())
-      .then(console.log)
+      .then((data: QuestionI[]) => {
+        dispatch({ type: "dataReceived", payload: data });
+        console.log(data);
+      })
       .catch(console.error);
   }, []);
 

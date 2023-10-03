68dfd3131818a6dec6f15fc692b1f96be405438c
Hashib Raja <hashibraja23@gmail.com>
2023-10-03 14:27:51 +0530

    Added starting files

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import Header from "./Header";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 };
 
 export default App;
---
+++ src/Header.tsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <header className="app-header">
+      <img
+        src="https://the-react-quiz.netlify.app/logo512.png"
+        alt="React logo"
+      />
+      <h1>The React Quiz</h1>
+    </header>
+  );
+}
+
+export default Header;

114f57c8e987ecefe010e6cec255475889ca0cd8
Hashib Raja <hashibraja23@gmail.com>
2023-10-04 07:16:05 +0530

    Added logo in local 🛐🛐

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,10 +1,7 @@
 function Header() {
   return (
     <header className="app-header">
-      <img
-        src="https://the-react-quiz.netlify.app/logo512.png"
-        alt="React logo"
-      />
+      <img src="logo.png" alt="React logo" />
       <h1>The React Quiz</h1>
     </header>
   );
